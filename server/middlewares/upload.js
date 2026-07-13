09ccbb10e7694226ec02e74a0a5459056a8dee30
Roshan4546 <roshanpatra275@gmail.com>
2026-07-13 09:04:25 +0530

    implement file upload middleware with multer and configure Cloudinary integration

---
+++ server/middlewares/upload.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'application/pdf' ||
+        file.mimetype === 'application/msword' ||
+        file.mimetype === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    ) {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type. Only PDF and WORD document are allowed'));
+    }
+};
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 5 * 1024 * 1024 }
+});
+
+module.exports = upload;
